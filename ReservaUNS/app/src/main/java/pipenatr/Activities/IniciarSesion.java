0e0a027cf05cb99023d535a594f4a6006ba8ba34
EnriqueViolaGómez <enriqueviola2014@gmail.com>
2018-08-10 14:53:07 -0300

    esplotado

--- ReservaUNS/app/src/main/java/pipenatr/Activities/IniciarSesion.java
+++ ReservaUNS/app/src/main/java/pipenatr/Activities/IniciarSesion.java
@@ -3,10 +3,12 @@ package pipenatr.Activities;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -47,13 +49,6 @@ public class IniciarSesion extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -328,16 +323,13 @@ public class IniciarSesion extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
+            if(controller.verificarLogIn(mEmail,mPassword))
+                return true;
+            else
+                mostrarMensajeError("No se pudo iniciar sesión, por favor verifique el email y contraseña ingresados.");
             // TODO: register the new account here.
-            return true;
+
+            return false;
         }
 
         @Override
@@ -367,6 +359,21 @@ public class IniciarSesion extends AppCompatActivity implements LoaderCallbacks<
         Intent intent = new Intent(this, PantallaPrincipal.class);
         finish();
         startActivity(intent);
+    }
+
+    public void mostrarMensajeError(String mensaje)
+    {
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Cierra la ventana
+            }
+        });
+        alerta.setMessage(mensaje);
+        alerta.setTitle("Error");
+        alerta.setCancelable(true);
+        alerta.create().show();
     }
 }
 
